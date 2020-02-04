b01c9c62ddf8b72deaadf225f510f2e1e36a407b
connellw <connellw@users.noreply.github.com>
2020-02-04 17:43:15 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Autofac;
+using Autofac.Extras.DynamicProxy;
+
+namespace AutofacDynamicProxyBug
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Thingy>()
+                   .As<IThingy>()
+                   .EnableInterfaceInterceptors();
+
+            var container = builder.Build();
+
+            var thingy = container.Resolve<IThingy>();
+        }
+    }
+
+    public class Thingy : IThingy { }
+
+    public interface IThingy { }
+}
